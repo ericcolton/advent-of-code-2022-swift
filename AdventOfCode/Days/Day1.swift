65682ad4c8ae9b40e43eaa812b1805952cbbcb7f
Eric Colton <ericcolton@gmail.com>
2022-12-01 22:13:10 -0500

    2022 day 1 complete

--- AdventOfCode/Days/Day1.swift
+++ AdventOfCode/Days/Day1.swift
@@ -7,22 +7,37 @@ import Algorithms
 import Foundation
 
 final class Day1: Day {
+  
+  enum ValidationError: Error {
+    case unexpectedLine
+  }
+  
+  
+  func buildAndSortElfs (_ input: String) throws -> [Int]
+  {
+    var elfs : [Int] = []
+    var elf = 0
+    for i in input.split(separator: "\n", omittingEmptySubsequences: false) {
+      if let i = Int(i) {
+        elf += i
+      } else if i == "" {
+        elfs.append(elf)
+        elf = 0
+      } else {
+        throw ValidationError.unexpectedLine
+      }
+    }
+    if elf > 0 {
+      elfs.append(elf)
+    }
+    return elfs.sorted()
+  }
+  
     func part1(_ input: String) -> CustomStringConvertible {
-        return input
-            .split(separator: "\n")
-            .compactMap { Int($0) }
-            .adjacentPairs()
-            .count { $0 < $1 }
+      return try! buildAndSortElfs(input).last!
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return input
-            .split(separator: "\n")
-            .compactMap { Int($0) }
-            .windows(ofCount: 3)
-            .adjacentPairs()
-            .count {
-                $0.sum < $1.sum
-            }
+      return try! buildAndSortElfs(input).suffix(3).reduce(0, +)
     }
 }
