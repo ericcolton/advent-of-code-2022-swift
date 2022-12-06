bafd039f606fb500461f6c981cacedca298641ab
Eric Colton <ericcolton@gmail.com>
2022-12-06 04:26:54 -0500

    added 2022 02

--- AdventOfCode/Days/Day2.swift
+++ AdventOfCode/Days/Day2.swift
@@ -6,47 +6,57 @@
 import Foundation
 
 final class Day2: Day {
-    func part1(_ input: String) -> CustomStringConvertible {
-        var x = 0
-        var y = 0
-        input
-            .split(separator: "\n")
-            .forEach { line in
-                let command = line.components(separatedBy: " ")
-                switch command[0] {
-                case "forward":
-                    x += Int(command[1]) ?? 0
-                case "down":
-                    y += Int(command[1]) ?? 0
-                case "up":
-                    y -= Int(command[1]) ?? 0
-                default:
-                    break
-                }
-            }
-        return x * y
+  
+  let ASCII_A = 65
+  let ASCII_X = 88
+  
+  func scoreAsPlay(_ input : String) -> Int
+  {
+    let pair = input.components(separatedBy: " ")
+    let a = Int(Character(pair[0]).asciiValue!) - ASCII_A
+    let b = Int(Character(pair[1]).asciiValue!) - ASCII_X
+    if a == b {
+      return 3 + b + 1
+    } else if b == (a + 1) % 3 {
+      return 6 + b + 1
+    } else {
+      return 0 + b + 1
+    }
+  }
+  
+  func determineB(_ a: Int, _ outcome: Int) -> Int
+  {
+    if outcome == 0 {
+      if a == 0 {
+        return 2
+      }
+      return (a - 1) % 3
+    } else if outcome == 3 {
+      return a
+    }
+    return (a + 1) % 3
+  }
+  
+  func scoreAsOutcome(_ input : String) -> Int
+  {
+    let pair = input.components(separatedBy: " ")
+    let a = Int(Character(pair[0]).asciiValue!) - ASCII_A
+    let rawOutcome = Int(Character(pair[1]).asciiValue!) - ASCII_X
+    let outcome = 3 * rawOutcome
+    return outcome + determineB(a, outcome) + 1
+  }
+  
+  func part1(_ input: String) -> CustomStringConvertible {
+    return input
+      .split(separator: "\n")
+      .map { scoreAsPlay(String($0)) }
+      .reduce(0, +)
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        var x = 0
-        var y = 0
-        var aim = 0
-        input
-            .split(separator: "\n")
-            .forEach { line in
-                let command = line.components(separatedBy: " ")
-                switch command[0] {
-                case "forward":
-                    x += Int(command[1]) ?? 0
-                    y += (Int(command[1]) ?? 0) * aim
-                case "down":
-                    aim += Int(command[1]) ?? 0
-                case "up":
-                    aim -= Int(command[1]) ?? 0
-                default:
-                    break
-                }
-            }
-        return x * y
+      return input
+        .split(separator: "\n")
+        .map { scoreAsOutcome(String($0)) }
+        .reduce(0, +)
     }
 }
